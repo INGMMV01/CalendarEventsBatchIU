6d4b2b9b29eaebd8528bfe958520c97a8f10c805
INGMMV01 <kitos78@gmail.com>
2017-11-04 23:37:35 +0100

    primer objetivo
    -selección de calendarios
    -movimiento de eventos non all-day
    -log

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,11 +29,21 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.lbTodos = new System.Windows.Forms.ListBox();
-            this.lbElegidos = new System.Windows.Forms.ListBox();
-            this.btnSeleccionar = new System.Windows.Forms.Button();
             this.btnDescartar = new System.Windows.Forms.Button();
+            this.btnSeleccionar = new System.Windows.Forms.Button();
+            this.lbElegidos = new System.Windows.Forms.ListBox();
+            this.lbTodos = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -44,54 +54,138 @@
             this.groupBox1.Controls.Add(this.lbTodos);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(686, 340);
+            this.groupBox1.Size = new System.Drawing.Size(611, 176);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Calendarios";
+            // 
+            // btnDescartar
+            // 
+            this.btnDescartar.Location = new System.Drawing.Point(251, 89);
+            this.btnDescartar.Name = "btnDescartar";
+            this.btnDescartar.Size = new System.Drawing.Size(75, 23);
+            this.btnDescartar.TabIndex = 3;
+            this.btnDescartar.Text = "Descartar";
+            this.btnDescartar.UseVisualStyleBackColor = true;
+            this.btnDescartar.Click += new System.EventHandler(this.btnDescartar_Click);
+            // 
+            // btnSeleccionar
+            // 
+            this.btnSeleccionar.Location = new System.Drawing.Point(251, 42);
+            this.btnSeleccionar.Name = "btnSeleccionar";
+            this.btnSeleccionar.Size = new System.Drawing.Size(75, 23);
+            this.btnSeleccionar.TabIndex = 2;
+            this.btnSeleccionar.Text = "Seleccionar";
+            this.btnSeleccionar.UseVisualStyleBackColor = true;
+            this.btnSeleccionar.Click += new System.EventHandler(this.btnSeleccionar_Click);
+            // 
+            // lbElegidos
+            // 
+            this.lbElegidos.FormattingEnabled = true;
+            this.lbElegidos.Location = new System.Drawing.Point(353, 20);
+            this.lbElegidos.Name = "lbElegidos";
+            this.lbElegidos.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.lbElegidos.Size = new System.Drawing.Size(211, 134);
+            this.lbElegidos.TabIndex = 1;
             // 
             // lbTodos
             // 
             this.lbTodos.FormattingEnabled = true;
             this.lbTodos.Location = new System.Drawing.Point(7, 20);
             this.lbTodos.Name = "lbTodos";
-            this.lbTodos.Size = new System.Drawing.Size(211, 277);
+            this.lbTodos.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.lbTodos.Size = new System.Drawing.Size(211, 134);
             this.lbTodos.TabIndex = 0;
             // 
-            // lbElegidos
+            // groupBox2
             // 
-            this.lbElegidos.FormattingEnabled = true;
-            this.lbElegidos.Location = new System.Drawing.Point(336, 20);
-            this.lbElegidos.Name = "lbElegidos";
-            this.lbElegidos.Size = new System.Drawing.Size(211, 277);
-            this.lbElegidos.TabIndex = 1;
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.dateTimePicker2);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.dateTimePicker1);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 195);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(611, 57);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Fechas";
             // 
-            // btnSeleccionar
+            // label1
             // 
-            this.btnSeleccionar.Location = new System.Drawing.Point(239, 107);
-            this.btnSeleccionar.Name = "btnSeleccionar";
-            this.btnSeleccionar.Size = new System.Drawing.Size(75, 23);
-            this.btnSeleccionar.TabIndex = 2;
-            this.btnSeleccionar.Text = "Seleccionar";
-            this.btnSeleccionar.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Desde";
             // 
-            // btnDescartar
+            // dateTimePicker1
             // 
-            this.btnDescartar.Location = new System.Drawing.Point(239, 154);
-            this.btnDescartar.Name = "btnDescartar";
-            this.btnDescartar.Size = new System.Drawing.Size(75, 23);
-            this.btnDescartar.TabIndex = 3;
-            this.btnDescartar.Text = "Descartar";
-            this.btnDescartar.UseVisualStyleBackColor = true;
+            this.dateTimePicker1.Location = new System.Drawing.Point(52, 20);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(313, 20);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(268, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Hasta";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(530, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Mover";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.richTextBox1);
+            this.groupBox3.Location = new System.Drawing.Point(12, 259);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(611, 299);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Resultados";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(10, 20);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(598, 273);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(732, 620);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "CalendarEventsBatchIU";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -103,6 +197,14 @@
         private System.Windows.Forms.Button btnSeleccionar;
         private System.Windows.Forms.ListBox lbElegidos;
         private System.Windows.Forms.ListBox lbTodos;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,20 +1,175 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System.IO;
+using System.Threading;
 
 namespace CalendarEventsBatchV2
 {
     public partial class Form1 : Form
     {
+
+        // If modifying these scopes, delete your previously saved credentials
+        // at ~/.credentials/calendar-dotnet-quickstart.json
+        //kitos static string[] Scopes = { CalendarService.Scope.CalendarReadonly };
+        static string[] Scopes = { CalendarService.Scope.Calendar };
+        static string ApplicationName = "Google Calendar API .NET Quickstart";
+        System.Collections.Hashtable htCalendars = new System.Collections.Hashtable();
+        CalendarService objServicio = null;
+        System.Text.StringBuilder stringbuilder = new System.Text.StringBuilder();
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ObtenerCalendarios();
+        }
+
+        private void ObtenerCalendarios()
+        {
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = System.Environment.GetFolderPath(
+                    System.Environment.SpecialFolder.Personal);
+                credPath = Path.Combine(credPath, ".credentials/calendar-dotnet-quickstart.json");
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Google Calendar API service.
+            var service = new CalendarService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+            objServicio = service;
+
+            CalendarListResource.ListRequest requestListCalendar = service.CalendarList.List();
+
+            CalendarList calendars = requestListCalendar.Execute();
+            foreach (CalendarListEntry cal in calendars.Items)
+            {
+                System.Drawing.Color color = System.Drawing.Color.FromName(cal.ColorId);
+                this.lbTodos.ForeColor = color;
+                this.lbTodos.Items.Add(cal.Summary);
+                this.htCalendars.Add(cal.Summary, cal);
+            }            
+        }
+
+        private void btnSeleccionar_Click(object sender, EventArgs e)
+        {
+            MoverItemListBox(lbTodos, lbElegidos);
+        }
+
+        private void btnDescartar_Click(object sender, EventArgs e)
+        {
+            MoverItemListBox(lbElegidos, lbTodos);
+        }
+
+        private void MoverItemListBox(ListBox origen, ListBox destino)
+        {
+            System.Collections.ArrayList itemsEliminar = new System.Collections.ArrayList();
+            foreach (Object item in origen.SelectedItems)
+            {
+                destino.Items.Add(item);
+                itemsEliminar.Add(item);
+            }
+
+            foreach(object obj in itemsEliminar)
+            {
+                origen.Items.Remove(obj);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            stringbuilder = new System.Text.StringBuilder();
+            richTextBox1.Text = String.Empty;
+
+            foreach (string nombreCalendario in lbElegidos.Items)
+            {
+                CalendarListEntry cal = (CalendarListEntry)htCalendars[nombreCalendario];                
+
+                Events eventos = ObtenerEventos(cal);
+                if (eventos.Items != null && eventos.Items.Count > 0)
+                {
+                    foreach (var eventItem in eventos.Items)
+                    {
+                        ModificarEvento(cal, eventItem);
+                    }
+                }
+            }
+
+            richTextBox1.Text = stringbuilder.ToString();
+        }
+
+        private Events ObtenerEventos(CalendarListEntry cal)
+        {
+            // Define parameters of request.
+            EventsResource.ListRequest request = objServicio.Events.List(cal.Id);
+            request.TimeMin = this.dateTimePicker1.Value.Date;
+            request.TimeMax = this.dateTimePicker1.Value.Date.AddDays(1);
+            request.ShowDeleted = false;
+            request.SingleEvents = true;
+            request.MaxResults = 100;
+            request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
+            
+            Events events = request.Execute();
+            return events;
+        }
+
+        private void ModificarEvento(CalendarListEntry cal, Event eventItem)
+        {
+            TimeSpan ts = dateTimePicker2.Value - dateTimePicker1.Value;
+            int days = ts.Days;
+
+            string fechaHoraAntes = eventItem.Start.DateTime.ToString();
+
+            if (eventItem.Start.DateTime.HasValue)
+                eventItem.Start.DateTime = eventItem.Start.DateTime.Value.AddDays(days);
+            else
+            {
+                return;
+
+                //todo: no rula esta asignación
+                DateTime dt = DateTime.Parse(eventItem.Start.Date);
+                //eventItem.Start.Date = dt.AddDays(days).Date.ToString();
+                eventItem.Start.DateTime = dt.AddDays(days);
+            }
+
+            if (eventItem.End.DateTime.HasValue)
+                eventItem.End.DateTime = eventItem.End.DateTime.Value.AddDays(days);
+            else
+            {
+                return;
+
+                //todo: no rula esta asignación
+                DateTime dt = DateTime.Parse(eventItem.End.Date);
+                //eventItem.End.Date = dt.AddDays(days).Date.ToString();
+                eventItem.End.DateTime = dt.AddDays(days);
+            }
+            
+            EventsResource.PatchRequest patchRequest = objServicio.Events.Patch(eventItem, cal.Id, eventItem.Id);
+            Event event2 = patchRequest.Execute();
+            
+            stringbuilder.AppendLine(String.Format("{0} - {1}: ({2} a {3})", cal.Summary, event2.Summary, fechaHoraAntes, event2.Start.DateTime.ToString()));
+        }
+
     }
 }
